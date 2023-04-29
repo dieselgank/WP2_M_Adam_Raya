5c62345e84edec53d85e9ec60282dea543c42e6b
dieselgank <adamraya11@gmail.com>
2023-04-29 18:45:30 +0700

    menambah about.html

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -1,14 +1,21 @@
 <?php
 defined('BASEPATH') or exit ('no direct script access allowed');
 class Web extends CI_Controller{
-   function __construct(){
-       parent::__construct();
-       
-   }
-   public function index(){
-       $data['judul'] = "Halaman Depan";
-       $this->load->view('v_header', $data);
-       $this->load->view('v_index', $data);
-       $this->load->view('v_footer', $data);
-   }
+ function __construct(){
+     parent::__construct();
+     $this->load->helper('url');
+ }
+ public function index(){
+     $data['judul'] = "Halaman Depan";
+     $this->load->view('v_header', $data);
+     $this->load->view('v_index', $data);
+     $this->load->view('v_footer', $data);
+ }
+ public function about()
+ {
+    $data['judul'] = "Halaman Depan";
+    $this->load->view('v_header', $data);
+    $this->load->view('v_about', $data);
+    $this->load->view('v_footer', $data);
+}
 }
