b8dfe013ffad6df9da0cbc760f3d1dc654c64b8a
dieselgank <adamraya11@gmail.com>
2023-06-12 02:42:05 +0700

    pertemuan 11 dan 12

---
+++ application/views/buku/ubah_kategori.php
@@ -0,0 +1,22 @@
+<div class="container-fluid">
+    <?= $this->session->flashdata('pesan'); ?>
+    <div class="row"><div class="col-lg-3">
+        <?php if(validation_errors()){
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message role="alert">Nama Kategori tidak boleh kosong</div>');
+            redirect('buku/ubahKategori/' .$k['id']);
+        } ?>
+        <?php foreach ($kategori as $k) { ?>
+            <form action="<?= base_url('buku/ubahKategori'); ?>" method="post">
+                <div class="form-group">
+                    <input type="hidden" name="id" id="id" value="<?php echo $k['id']; ?>">
+                    <input type="text" class="form-control form-control-user" id="kategori" name="kategori" placeholder="Masukkan Kategori Buku" value="<?= $k['kategori']; ?>">
+                </div>
+                <div class="form-group">
+                    <input type="button" class="form-control form-control-user btn btn-dark col-lg-4 mt-3" value="Kembali" onclick="window.history.go(-1)">
+                    <input type="submit" class="form-control form-control-user btn btn-primary col-lg-4 mt-3" value="Update">
+                </div>
+            </form>
+            <?php } ?>
+        </div>
+    </div>
+</div>
